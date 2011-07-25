9b820f9db9379ee084fc3ceaa5d6e73ab7bc7a23
Matthew Clark <mclark4386@gmail.com>
2011-07-25 17:30:23 -0500

    Initial commit (barebones rails 3.1)

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CloudDnD::Application.config.secret_token = '1b39e064c0b7e14f6000af04110a213223a3874e86121ae11e7d4ad2ce375e2070913d79753e8a58526b725722a6d6f209298b059846a564643e18a1fd123c48'
